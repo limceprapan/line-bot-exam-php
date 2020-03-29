3c49e8530e2805653f32ab43f7b20dfbdf1bd9cb
Wachira Limsripraphan <limceprapan@gmail.com>
2020-03-29 23:17:32 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,9 +3,9 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$// access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+// access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
 
-access_token = 'FL3ty93Uh/SwOTH6Q3IscEJHwhHdUaDaZSYoGCDrHtht4X7KQ+BnUHriU+Fcob1JcEoI+EjBgTDQCZRSJlYwDRN3/1tBICEY2ZCUEtRmaeyC+D5qhIh1NeIKcMLqV9ow8BbU2G8izJTQwo1Y7DRYXQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FL3ty93Uh/SwOTH6Q3IscEJHwhHdUaDaZSYoGCDrHtht4X7KQ+BnUHriU+Fcob1JcEoI+EjBgTDQCZRSJlYwDRN3/1tBICEY2ZCUEtRmaeyC+D5qhIh1NeIKcMLqV9ow8BbU2G8izJTQwo1Y7DRYXQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
